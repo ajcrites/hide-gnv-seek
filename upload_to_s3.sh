c917a254d644a4073c7882e4a19a5d042f6c8c68
Bjorn Stange <bstange@mobiquityinc.com>
2017-08-26 16:54:42 -0400

    updating upload script again again

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -3,4 +3,5 @@
 aws s3 cp index.html s3://hide-gnv-seek/index.html --profile hide
 aws s3 cp favicon.ico s3://hide-gnv-seek/favicon.ico --profile hide
 aws s3 cp style.css s3://hide-gnv-seek/style.css --profile hide
+aws s3 cp src/hidegnvseek3.png s3://hide-gnv-seek/src/hidegnvseek3.png --profile hide
 aws s3 cp --recursive dist s3://hide-gnv-seek/dist --profile hide
